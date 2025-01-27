c2119a707ac32a2b78726d33b4ef503656cb672c
reeri15 <reshay.makunyane@gmail.com>
2025-01-27 11:59:49 +0200

    database added

---
+++ client /pop/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+
+import { ProductFormComponent } from './components/product-form/product-form.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'products', component: ProductListComponent },
+  { path: 'add', component: ProductFormComponent },
+  { path: 'edit/:id', component: ProductFormComponent }
+];
